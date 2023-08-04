6ba42c2455e274fed9b1f338b741f293279189c3
rohitf116 <rrsonawne090909@gmail.com>
2023-08-05 00:41:18 +0530

    tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import { ChatIcon } from "./svg";
 
 function App() {
-  return (
-    <div>
-      <h1>HIi</h1>
-      <ChatIcon />
-    </div>
-  );
+  return <div className="dark"></div>;
 }
 
 export default App;
